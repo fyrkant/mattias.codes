3ead0dc8c06dbfa60c1332efdeaa82bf85234833
Mattias Wikström <fyrkant@gmail.com>
2022-02-17 10:10:27 +0100

    chore: update to new serverBuildTarget config

--- remix.config.js
+++ remix.config.js
@@ -2,11 +2,14 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
   serverBuildTarget: "netlify",
-  publicPath: "/build/",
-  serverBuildPath: "netlify/functions/server/build",
-  devServerPort: 8002,
+  server: "./server.js",
   ignoredRouteFiles: [".*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildTarget: "netlify",
+  // publicPath: "/build/",
+  // serverBuildDirectory: "netlify/functions/server/build",
+  // devServerPort: 8002,
+  // ignoredRouteFiles: [".*"],
 };
